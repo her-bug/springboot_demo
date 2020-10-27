ddd40fce56dba70813a98e5bde61e324d0a1a20f
offcn-java <18235275264@163.com>
2020-10-27 20:30:09 +0800

    123

--- src/main/java/com/offcn/bean/User.java
+++ src/main/java/com/offcn/bean/User.java
@@ -1,16 +1,8 @@
 package com.offcn.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
 import java.io.Serializable;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ConfigurationProperties(prefix = "user")
+
 public class User implements Serializable {
 
     private String name;
